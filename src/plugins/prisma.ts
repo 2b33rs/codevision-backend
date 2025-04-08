7ecae2d4e2aa1a7091df9eed7fa7318be683ca06
2b33rs <tobiasbiehl@icloud.com>
2025-04-08 21:31:10 +0200

    modify cors origin

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,11 +1,6 @@
 import fp from 'fastify-plugin'
 import { PrismaClient } from '../../generated/prisma'
 
-const databaseUrl =
-  process.env.NODE_ENV === 'test'
-    ? process.env.DATABASE_URL_TEST
-    : process.env.DATABASE_URL
-
 export const prisma = new PrismaClient()
 
 export default fp(async (fastify) => {
--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,11 @@ export function createServer() {
   const app = Fastify({ ignoreTrailingSlash: true, logger: true })
 
   app.register(cors, {
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://codevision.up.railway.app',
+      'http://localhost:80',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   })
 
