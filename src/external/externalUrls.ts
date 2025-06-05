02d7590f941e57550f499e71d25b2e74c35ee7ad
Kevin Balaban <kevlar6998@gmail.com>
2025-06-05 09:02:03 +0200

    external apis added

--- src/external/externalUrls.ts
+++ src/external/externalUrls.ts
@@ -1,4 +1,3 @@
-export const MAWI_API_URL = process.env.MAWI_API_URL ?? 'http://localhost:8082'
-
+export const MAWI_API_URL = process.env.MAWI_API_URL ?? 'https://mawi-backend-atfhg6amfbcfgmd5.swedencentral-01.azurewebsites.net'
 export const PRODUCTION_API_URL =
-  process.env.PRODUCTION_API_URL ?? 'http://localhost:8081'
+  process.env.PRODUCTION_API_URL ?? 'https://backend-your-shirt-gmbh-production.up.railway.app/'
