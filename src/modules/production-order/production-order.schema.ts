554b5fd1bfbd7d7fc69f702c99f6dd7d1d698ca5
KurtEvlar <kevin_balaban@web.de>
2025-06-07 19:02:56 +0200

    master merge and routes and schema for prodorder

---
+++ src/modules/production-order/production-order.schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+import { zodToJsonSchema } from 'zod-to-json-schema'
+import { $Enums } from '../../../generated/prisma'
+
+export const createProductionOrderZ = z.object({
+  positionId: z.string().uuid(),
+  amount: z.number().int().positive(),
+  designUrl: z.string(),
+  orderType: z.string(),
+  dyeingNecessary: z.boolean(),
+  productTemplate: z.any(),
+  Status: z.nativeEnum($Enums.PRODUCTION_ORDER_STATUS).optional(),
+  deletedAt: z.string().datetime().nullable().optional(),
+})
